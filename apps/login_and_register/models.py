b397882d7c3d8baa7874e2f3d5dc01162616caa2
Kevin Marinas <marinaskevin@outlook.com>
2018-03-26 12:16:50 -0700

    Creating Ping Pong App for Python Project Week at Coding Dojo

---
+++ apps/login_and_register/models.py
@@ -0,0 +1,81 @@
+from __future__ import unicode_literals
+from django.db import models
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+NAME_REGEX = re.compile(r'[a-zA-Z]{2}')
+PASSWORD_REGEX = re.compile(r'[a-zA-Z0-9 !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"]{8}')
+import bcrypt
+
+# Create your models here.
+class UserManager(models.Manager):
+    def validateRegistration(self, postData):
+
+        feedback = {
+            'first_name': postData['first_name'],
+            'last_name': postData['last_name'],
+            'email': postData['email']
+        }
+
+        result = {
+             'status': False
+        }
+
+        errors = []
+
+        if len(postData['first_name']) < 1:
+            errors.append('Please enter your first name (2 or more letters only).')
+            feedback["first_name"] = ""
+        elif len(postData['first_name']) < 2:
+            errors.append('First name must have least 2 characters.')
+            feedback["first_name"] = ""
+        elif not NAME_REGEX.match(postData['first_name']):
+            errors.append('First name must be letters only.')
+            feedback["first_name"] = ""
+        if len(postData['last_name']) < 1:
+            errors.append('Please enter your last name (2 or more letters only).')
+            feedback["last_name"] = ""
+        elif len(postData['last_name']) < 2:
+            errors.append('Last name must have least 2 characters.')
+            feedback["last_name"] = ""
+        elif not NAME_REGEX.match(postData['last_name']):
+            errors.append('Last name must be letters only.')
+            feedback["last_name"] = ""
+        if len(postData['email']) < 1:
+            errors.append('Please enter an email.')
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors.append('Invalid Email.')
+        if User.objects.filter(email=postData['email']).count() > 0:
+            errors.append('Account '+postData['email']+' is already registered.')
+            feedback["email"] = ""
+        if len(postData['password']) < 1:
+            errors.append('Please enter a password (8 or more characters).')
+        elif len(postData['cpassword']) < 1:
+            errors.append('Please confirm your password.')
+        elif not PASSWORD_REGEX.match(postData['password']):
+            errors.append('Password must have 8 or more characters.')
+        if postData['password'] != postData['cpassword']:
+            errors.append('Passwords do not match.')
+
+        if len(errors):
+            result['errors'] = errors
+
+        else:
+            result['status'] = True
+            pw_hash = bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt())
+            user = User.objects.create(first_name = postData['first_name'],last_name = postData['last_name'],email = postData['email'],pw_hash = pw_hash)
+            result['user_id'] = user.id
+
+        result['feedback'] = feedback        
+
+        print result
+        return result
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    pw_hash = models.CharField(max_length=255)
+    objects = UserManager()
+    def __str__(self):
+        return self.first_name,self.last_name
---
+++ apps/login_and_register/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from django.contrib.messages import get_messages
+from models import *
+
+# Create your views here.
+def index(request):
+    if 'id' not in request.session:
+        request.session['id'] = 0
+    context = {
+        'user': User.objects.filter(id = request.session['id']).first()
+    }
+    return render(request,'login_and_register/index.html',context)
+
+def login(request):
+    if request.method != 'POST':
+        return redirect('./')
+
+    errors = []
+
+    email = request.POST['email']
+    # pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+
+    if len(User.objects.filter(email=email)) > 0:
+        if bcrypt.checkpw(request.POST['password'].encode(), User.objects.filter(email=email).first().pw_hash.encode()):
+            print "Password MATCHES"
+            user = User.objects.filter(email=email).first()
+            request.session["id"] = user.id
+            messages.success(request, 'Successfully logged in!')
+            return redirect('./success')
+        else:
+            print "Password is incorrect!"
+            messages.error(request, 'Password is incorrect.')
+    else:
+        print "User does not exist!"
+        messages.error(request, 'User does not exist.')    
+    return redirect('./')
+
+def logout(request):
+    request.session.clear()
+    return redirect('./')
+
+def success(request):
+    no_messages = True
+    messages = get_messages(request)
+    for message in messages:
+        no_messages = False
+        break
+    if 'id' not in request.session or no_messages:
+        return redirect('./')
+    context = {
+        'user': User.objects.filter(id = request.session['id']).first()
+    }
+    return render(request,'login_and_register/success.html',context)
+
+def register(request):
+    if request.method != 'POST':
+        return redirect('./')
+    response = User.objects.validateRegistration(request.POST)
+    if response['status']:
+        messages.success(request,'Registration Successful!')
+        request.session.clear()
+        request.session["id"] = response['user_id']
+        return redirect('./success')
+    if 'errors' in response:
+        for error in response['errors']:
+            messages.error(request,error)
+        for key in response['feedback']:
+            request.session[key] = response['feedback'][key]
+        return redirect('./')
