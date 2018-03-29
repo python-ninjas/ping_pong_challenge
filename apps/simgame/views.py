7b8f78a8df96e1208ef69c5adc421b38edd3c530
Michael White <engiemcwhite@gmail.com>
2018-03-29 13:16:31 -0500

    Added validations and dummy opponent

--- apps/simgame/views.py
+++ apps/simgame/views.py
@@ -8,9 +8,11 @@ import random
 # Create your views here.
 def index(request):
     user = User.objects.filter(id = request.session['id']).first()
+    upperbound = user.skill * 4
+    lowerbound = user.skill / 4
     context = {
         'user': user,
-        'opponents': User.objects.exclude(id=request.session['id']).order_by("-skill")
+        'opponents': User.objects.exclude(id=request.session['id'], skill__gt = upperbound, skill__lt = lowerbound).order_by("-skill")
     }
     return render(request,'simgame/index.html',context)
 
@@ -20,49 +22,60 @@ def play(request):
     p1points = 0
     p2points = 0 #Player making the challenge is always player 1. 
     x = User.objects.get(id=int(request.session['id']))
-    y = User.objects.get(id=int(request.session['opp_id']))
     p1skill = float(x.skill)
-    p2skill = float(y.skill)
-    while abs(p1points - p2points) < 2 and p1points < request.session['max_points'] and p2points < request.session['max_points']:
+    if request.POST['opp_id'] == "dummy":
+        p2skill = p1skill
+    else: 
+        y = User.objects.get(id=int(request.session['opp_id']))
+        p2skill = float(y.skill)
+    while abs(p1points - p2points) < 2 or (p1points < request.session['max_points'] and p2points < request.session['max_points']):
         chance = p1skill/(p1skill + p2skill)
-        rand_num = random.random();
+        rand_num = random.random()
         if rand_num < chance: 
             p1points += 1
         else: 
             p2points += 1
+    
     x.totalscore += p1points
     x.experience += p1points
-    y.totalscore += p2points
-    y.experience += p2points
+    if request.POST['opp_id'] != "dummy":
+        y.totalscore += p2points
+        y.experience += p2points
     if p1points > p2points:
-        win_exp = 10*y.skill
-        lose_exp = 2*x.skill
-        x.experience += win_exp
-        y.experience += lose_exp
-        Game.objects.create(winner=x, loser=y,points_win=p1points,points_lose=p2points,win_exp = win_exp, lose_exp = lose_exp)
+        if(request.POST['opp_id'] != "dummy"):
+            win_exp = 10*y.skill
+            lose_exp = 2*x.skill
+            x.experience += win_exp
+            y.experience += lose_exp
+            Game.objects.create(winner=x, loser=y,points_win=p1points,points_lose=p2points,win_exp = win_exp, lose_exp = lose_exp)
+        else:
+            win_exp = 5 * x.skill
+            lose_exp = 1 * x.skill
+            x.experience += win_exp
+            Game.objects.create(winner=x,points_win=p1points,points_lose=p2points,win_exp = win_exp, lose_exp = lose_exp)
         messages.success(request,'You win! Congratulations!')
     else: 
-<<<<<<< HEAD
-        lose_exp = 2*y.skill
-        win_exp = 10*x.skill
-        x.experience += lose_exp
-        y.experience += win_exp
-        Game.objects.create(winner=y, loser=x,points_win=p2points,points_lose=p1points,win_exp = win_exp, lose_exp = lose_exp)
+        if(request.POST['opp_id'] != "dummy"):
+            lose_exp = 2*y.skill
+            win_exp = 10*x.skill
+            x.experience += lose_exp
+            y.experience += win_exp
+            Game.objects.create(winner=y, loser=x,points_win=p2points,points_lose=p1points,win_exp = win_exp, lose_exp = lose_exp)
+        else:
+            lose_exp = x.skill
+            win_exp = 5*x.skill
+            x.experience+=lose_exp
+            Game.objects.create(loser=x,points_win=p2points,points_lose=p1points,win_exp = win_exp, lose_exp = lose_exp)
         messages.success(request,'You lose! Better luck next time!')
-=======
-        Game.objects.create(winner=y, loser=x,points_win=p2points,points_lose=p1points)
-        x.experience += 2 * y.skill
-        y.experience += 10 * x.skill
-        messages.error(request,'You lose! Better luck next time!')
->>>>>>> 76c5a8e6de50751db33e07055b043fab2b3caa71
     while x.experience >= x.skill * 50 + 1:
         x.experience -= x.skill * 50 + 1     #Level up! Accounts for gaining multiple levels in a single game, too. 
         x.skill += 1
-    while y.experience >= y.skill * 50 + 1:
-        y.experience -= y.skill * 50 + 1    
-        y.skill += 1
-    x.save()
-    y.save()
+        x.save()
+    if(request.POST['opp_id'] != "dummy"):
+        while y.experience >= y.skill * 50 + 1:
+            y.experience -= y.skill * 50 + 1    
+            y.skill += 1
+            y.save()
     if p1points > p2points:
         messages.success(request,'Score: {} to {}'.format(p1points,p2points))
     else:
